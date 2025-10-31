cdd7d0d3f6af192dfb8fcb2ce23fc9ad6401ac99
Anusha Yuvika <yuvikaanusha19@gmail.com>
2025-11-01 02:09:49 +0530

    Error Updated

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,16 @@ import "./../styles/App.css";
 const App = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("https://dummyjson.com/products")
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
       .then((result) => {
         setTimeout(() => { 
           setData(result);
@@ -15,14 +21,23 @@ const App = () => {
         }, 2000);
       })
       .catch((error) => {
-        console.error("Error fetching data:", error);
+        setError(err.message);
         setLoading(false);
       });
   }, []);
 
   return (
     <div id="main">
-      {loading ? <p>Loading...</p> : <pre>{JSON.stringify(data, null, 2)}</pre>}
+      {loading && <p>Loading...</p>}
+
+      {!loading && error && <p>An error occurred: {error}</p>}
+
+      {!loading && !error && (
+        <div>
+          <h1>Data Fetched from API</h1>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 };
